7e8c78472d17a5a62ee9aab38195f263da4e7f32
有这个人 <3354641260@qq.com>
2026-07-12 14:01:07 +0800

    Site updated: 2026-07-12 14:01:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/31/hello-world/","2026/07/07/Git 基础与核心操作 — 入门教程/","2026/07/12/基础与核心操作（下）/"];function toRandomPost(){
+var posts=["2026/03/31/hello-world/","2026/07/12/基础与核心操作（下）/","2026/07/07/Git 基础与核心操作 — 入门教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
